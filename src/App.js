09185f9c7d6f8dbb5965ca0d7e9be575dc6cec43
Divy <divyparekh1810@gmail.com>
2022-07-23 19:11:30 +0530

    Initial layout is final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 
+//material ui
+import { CssBaseline, Grid } from "@mui/material";
+
+//components
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
+
 const App = () => {
-  console.log("Eh");
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
